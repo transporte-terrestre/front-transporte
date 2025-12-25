191d447d4e14ba998708fb9938166e7db72b71d3
erixcel <xerickcua@gmail.com>
2025-12-25 08:51:33 -0500

    .

--- src/modules/admin/layout/notificaciones/notificaciones.ts
+++ src/modules/admin/layout/notificaciones/notificaciones.ts
@@ -1,4 +1,4 @@
-import { Component, inject, computed } from '@angular/core';
+import { Component, inject, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificacionService } from '@service/admin/notificacion.service';
 
@@ -14,6 +14,9 @@ export class Notificaciones {
   isOpen = this.service.isOpen;
   notificaciones = this.service.notificaciones;
 
+  // Track which notifications are expanded
+  expandedIds = signal<Set<number>>(new Set());
+
   unreadCount = computed(() => this.notificaciones().filter((n) => !n.leido).length);
 
   close() {
@@ -27,5 +30,21 @@ export class Notificaciones {
 
   markAllRead() {
     this.service.markAllAsRead();
+  }
+
+  toggleExpand(id: number, event: Event) {
+    event.stopPropagation();
+    const current = this.expandedIds();
+    const newSet = new Set(current);
+    if (newSet.has(id)) {
+      newSet.delete(id);
+    } else {
+      newSet.add(id);
+    }
+    this.expandedIds.set(newSet);
+  }
+
+  isExpanded(id: number): boolean {
+    return this.expandedIds().has(id);
   }
 }
